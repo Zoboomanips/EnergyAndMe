d0f17f6d310606020966bfa1f7760bef4e7cd085
Zoboomanips <38367374+Zoboomanips@users.noreply.github.com>
2018-04-14 10:13:19 -0500

    Updated Again

--- app/src/main/java/bearsinchairs/energyandme/Node1Info.java
+++ app/src/main/java/bearsinchairs/energyandme/Node1Info.java
@@ -2,6 +2,7 @@ package bearsinchairs.energyandme;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
@@ -21,22 +22,30 @@ public class Node1Info extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_node1_info);
         power1 = 0;
-        port = 9331;
+        port = 9931;
         try {
             add = InetAddress.getByName("192.168.1.207");
         } catch (UnknownHostException e) {
 
         }
+
     }
 
-    protected void onStart(Bundle savedInstanceState) throws IOException{
+    /*protected void onStart(Bundle savedInstanceState) throws IOException{
         msg = "api/turnon/6";
         buf = msg.getBytes();
         DatagramSocket socket = new DatagramSocket();
         DatagramPacket packet = new DatagramPacket(buf, buf.length,
                 add, port);
         socket.send(packet);
+    }*/
 
-
+    public void turnOn (View view) throws IOException {
+        msg = "api/turnon/6";
+        buf = msg.getBytes();
+        DatagramSocket socket = new DatagramSocket();
+        DatagramPacket packet = new DatagramPacket(buf, buf.length,
+                add, port);
+        socket.send(packet);
     }
 }
