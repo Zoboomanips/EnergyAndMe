ca23321697dd60a57643019f1f10cc3c1d226f2a
Zoboomanips <38367374+Zoboomanips@users.noreply.github.com>
2018-04-13 21:29:13 -0500

    Initial commit

---
+++ app/src/main/java/bearsinchairs/energyandme/Node1Info.java
@@ -0,0 +1,15 @@
+package bearsinchairs.energyandme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Node1Info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_node1_info);
+        int power1 = 0;
+    }
+
+}
