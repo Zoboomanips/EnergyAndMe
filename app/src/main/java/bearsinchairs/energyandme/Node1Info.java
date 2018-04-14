79a4bb8ff1dfc47603d6210b0cb7e24ad30bd73e
Zoboomanips <38367374+Zoboomanips@users.noreply.github.com>
2018-04-14 09:17:23 -0500

    Updated AS

--- app/src/main/java/bearsinchairs/energyandme/Node1Info.java
+++ app/src/main/java/bearsinchairs/energyandme/Node1Info.java
@@ -1,15 +1,36 @@
 package bearsinchairs.energyandme;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 public class Node1Info extends AppCompatActivity {
 
+    int power1;
+    int port;
+    InetAddress add;
+    byte[] buf;
+    String msg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_node1_info);
-        int power1 = 0;
+        power1 = 0;
+        port = 9331;
+        add = InetAddress.getByName("192.168.1.207");
     }
 
+    protected void onStart(Bundle savedInstanceState) throws IOException{
+        msg = "api/getdcpower/0";
+        buf = msg.getBytes();
+        DatagramSocket socket = new DatagramSocket();
+        DatagramPacket packet = new DatagramPacket(buf, buf.length,
+                add, port);
+        socket.send(packet);
+
+    }
 }
