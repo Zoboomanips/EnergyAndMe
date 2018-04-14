0ff37028af3a8e2c189fcf020ab9924e7debca43
Zoboomanips <38367374+Zoboomanips@users.noreply.github.com>
2018-04-14 11:09:31 -0500

    New

--- app/src/main/java/bearsinchairs/energyandme/Node1Info.java
+++ app/src/main/java/bearsinchairs/energyandme/Node1Info.java
@@ -2,10 +2,12 @@ package bearsinchairs.energyandme;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.JsonReader;
 import android.view.View;
 
 import java.io.IOException;
 import java.net.*;
+import org.json.*;
 
 public class Node1Info extends AppCompatActivity {
 
@@ -20,25 +22,33 @@ public class Node1Info extends AppCompatActivity {
         setContentView(R.layout.activity_node1_info);
         power1 = 0;
         port = 9931;
-
+        try {
+            add = InetAddress.getByName("192.168.1.207");
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
     }
 
-    /*protected void onStart(Bundle savedInstanceState) throws IOException{
-        msg = "api/turnon/6";
-        buf = msg.getBytes();
-        DatagramSocket socket = new DatagramSocket();
-        DatagramPacket packet = new DatagramPacket(buf, buf.length,
-                add, port);
-        socket.send(packet);
-    }*/
-
     public void turnOn (View view) throws IOException {
-        add = InetAddress.getByName("192.168.1.207");
         msg = "api/turnon/6";
         buf = msg.getBytes();
         DatagramSocket socket = new DatagramSocket();
         DatagramPacket packet = new DatagramPacket(buf, buf.length,
                 add, port);
         socket.send(packet);
+    }
+
+    public double ping(String msg, int node) throws IOException, JSONException {
+        msg = "api/" + msg + "/" + node;
+        buf = msg.getBytes();
+        DatagramSocket socket = new DatagramSocket();
+        DatagramPacket packet = new DatagramPacket(buf, buf.length,
+                add, port);
+        socket.send(packet);
+        packet = new DatagramPacket(buf, buf.length);
+        socket.receive(packet);
+        String received = new String(packet.getData(), 0, packet.getLength());
+        JSONObject reader = new JSONObject(received);
+
     }
 }
